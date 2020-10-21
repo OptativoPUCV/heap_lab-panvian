4a5245fc63ba4d81d5201a19fb65fda6ea36a35b
repl.it user <replituser@example.com>
2020-10-21 20:57:48 +0000

    new try

--- heap.c
+++ heap.c
@@ -27,7 +27,7 @@ void* heap_top(Heap* pq){
 void heap_push(Heap* pq, void* data, int priority){
   if(pq->size==pq->capac) {
     pq->capac=pq->capac*2+1;
-    pq->heapArray=realloc(pq->heapArray,pq->capac);    
+    pq->heapArray=realloc(pq->heapArray,pq->capac* sizeof(heapElem));    
   }
   heapElem aux2;
   pq->heapArray[pq->size].priority=priority;
