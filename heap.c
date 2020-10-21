7911cddd54587a237179d75bb6234309e6315f30
repl.it user <replituser@example.com>
2020-10-21 20:17:52 +0000

    new try

--- heap.c
+++ heap.c
@@ -37,22 +37,16 @@ void heap_pop(Heap* pq){
   while(pq->heapArray[aux].priority<pq->heapArray[2*aux+1].priority || pq->heapArray[aux].priority<pq->heapArray[2*aux+2].priority) {
     
     if(pq->heapArray[2*aux+1].priority < pq->heapArray[2*aux+2].priority) {
-      aux2.priority=pq->heapArray[aux].priority;
-      aux2.data=pq->heapArray[aux].data;
-      pq->heapArray[aux].priority=pq->heapArray[2*aux+2].priority;
-      pq->heapArray[aux].data=pq->heapArray[2*aux+2].data;
-      pq->heapArray[2*aux+2].priority=aux2.priority;
-      pq->heapArray[2*aux+2].data=aux2.data;
+      aux2=pq->heapArray[aux];
+      pq->heapArray[aux]=pq->heapArray[2*aux+2];
+      pq->heapArray[2*aux+2]=aux2;
       aux=2*aux+2;
     }
     else {
-      aux2.priority=pq->heapArray[aux].priority;
-      aux2.data=pq->heapArray[aux].data;
-      pq->heapArray[aux].priority=pq->heapArray[2*aux+1].priority;
-      pq->heapArray[aux].data=pq->heapArray[2*aux+1].data;
-      pq->heapArray[2*aux+1].priority=aux2.priority;
-      pq->heapArray[2*aux+1].data=aux2.data;
-      aux=2*aux+2;
+      aux2=pq->heapArray[aux];
+      pq->heapArray[aux]=pq->heapArray[2*aux+1];
+      pq->heapArray[2*aux+1]=aux2;
+      aux=2*aux+1;
     }
   }
 }
